e1b7a19262d440d62c30247a5e4b28c822101eb8
Graham Cox <graham@grahamcox.co.uk>
2017-06-14 18:22:05 +0100

    Added in Cucumber for Acceptance Tests, but no features written yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,22 @@ module.exports = function (grunt) {
         express: {
             acceptance: {
                 options: {
-                    script: "src/server/index.js",
+                    script: 'src/server/index.js',
                     background: true,
-                    output: "Server listening on port"
+                    output: 'Server listening on port'
                 }
             }
-        }
+        },
+        shell: {
+            acceptance: {
+                command: 'cucumber.js src/acceptance/features -r src/acceptance/steps'
+            }
+		}
     });
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask('acceptance', ["express:acceptance"]);
+    grunt.registerTask('acceptance', ['express:acceptance', 'shell:acceptance']);
 };
