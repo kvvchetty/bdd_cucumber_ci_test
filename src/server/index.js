e5b9324b92e64dceb299da2fa521462af5dc64fd
Graham Cox <graham@grahamcox.co.uk>
2017-06-14 13:06:06 +0100

    Support for a secured endpoint using bearer tokens

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const passport = require('passport');
 const config = require('./config');
+require('./authentication/accesstoken');
 
 const app = express();
 
+app.use(passport.initialize());
+
 app.get('/api/insecure', (req, res) => {
-    res.send({
+	res.send({
 		answer: 42,
 		name: 'Graham'
 	});
 });
 
+app.get('/api/secure',
+	passport.authenticate('bearer', { session: false }),
+	(req, res) => {
+		res.send(req.user);
+	});
+
 const port = config.get('http.port');
 app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+	console.log('Server listening on port ' + port);
 });
