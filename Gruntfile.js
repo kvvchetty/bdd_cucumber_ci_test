61e0b7f3bde72013d9ddb30ace765c435eebef0b
Graham Cox <graham@grahamcox.co.uk>
2017-06-15 09:02:25 +0100

    Start the server on a random free port, instead of the default

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,28 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        freeport: {
+            acceptance: {
+                options: {
+                    start: 3000
+                }
+            }
+        },
         express: {
             acceptance: {
                 options: {
                     script: 'src/server/index.js',
                     background: true,
-                    output: 'Server listening on port'
+                    output: 'Server listening on port',
+                    port: '<%= freeport.acceptance %>'
                 }
             }
         },
         shell: {
             acceptance: {
-                command: 'cucumber.js src/acceptance/features -r src/acceptance/steps'
+                command: 'cucumber.js src/acceptance/features -r src/acceptance/steps',
+                env: {
+                    PORT: '<%= freeport.acceptance %>'
+                }
             }
 		}
     });
@@ -19,6 +30,7 @@ module.exports = function (grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-freeport');
 
-    grunt.registerTask('acceptance', ['express:acceptance', 'shell:acceptance']);
+    grunt.registerTask('acceptance', ['freeport:acceptance', 'express:acceptance', 'shell:acceptance']);
 };
